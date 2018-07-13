c3108de4a33933576e0f85c115cace70c5c598ee
Isao Sonobe <sonoisa@gmail.com>
2018-07-13 20:33:45 +0900

    CaboChaAnalyzerへの依存性をなくした。

--- pasa/parse/analyzer/analyzer.py
+++ pasa/parse/analyzer/analyzer.py
@@ -1,33 +1,37 @@
 # -*- coding: utf-8 -*-
 
-from cabocha.analyzer import CaboChaAnalyzer
+import CaboCha
 from pasa.result import Chunk, Morph, Result
 
 
 class Analyzer(object):
     def __init__(self):
-        self.analyzer = CaboChaAnalyzer()
+        self.analyzer = CaboCha.Parser()
 
     def parse(self, line):
         result = Result(line)
-        for chunk in self.analyzer.parse(line):
-            cid = chunk.id
-            clink = chunk.next_link_id
+        tree = self.analyzer.parse(line)
+        for i in range(tree.chunk_size()):
+            chunk = tree.chunk(i)
+            cid = i
+            clink = chunk.link
             chead = chunk.head_pos
             cfanc = chunk.func_pos
             cscore = chunk.score
             cchunk = Chunk(cid, clink, chead, cfanc, cscore)
             m_id = 0
-            for token in chunk:
+            for j in range(chunk.token_pos, chunk.token_pos + chunk.token_size):
+                token = tree.token(j)
                 surface = token.surface
-                pos = token.pos
-                pos1 = token.pos1
-                pos2 = token.pos2
-                pos3 = token.pos3
-                cform = token.cform
-                ctype = token.ctype
-                base = token.genkei
-                read = token.yomi
+                feature_list = token.feature_list
+                pos = feature_list(0)
+                pos1 = feature_list(1)
+                pos2 = feature_list(2)
+                pos3 = feature_list(3)
+                cform = feature_list(4)
+                ctype = feature_list(5)
+                base = feature_list(6)
+                read = feature_list(7) if token.feature_list_size >= 8 else surface
                 ne = token.ne
                 morph = Morph(m_id, surface, pos, pos1, pos2, pos3, cform, ctype, base, read, ne)
                 cchunk.addMorph(morph)
