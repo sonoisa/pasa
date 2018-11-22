cff2c99d1c055507745a86c731bdc499519711f4
Isao Sonobe <sonoisa@gmail.com>
2018-11-22 11:42:18 +0900

    バージョン番号誤記修正

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     author='Isao Sonobe',
     author_email='sonoisa@gmail.com',
     url='https://github.com/sonoisa/pasa/',
-    version='0.1.6',
+    version='0.1.7',
     install_requires=[],
     dependency_links=['https://github.com/taku910/cabocha/tree/master/python/'],
     zip_safe=True
