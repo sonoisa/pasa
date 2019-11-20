cd4ecb030e368afbb94427aa88af8f61d476b13b
Isao Sonobe <sonoisa@gmail.com>
2019-11-20 23:15:08 +0900

    慣用句解析や名詞複合述語の解析をオンオフするフラグを追加した。

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     author='Isao Sonobe',
     author_email='sonoisa@gmail.com',
     url='https://github.com/sonoisa/pasa/',
-    version='0.1.10',
+    version='0.1.11',
     install_requires=[],
     dependency_links=['https://github.com/taku910/cabocha/tree/master/python/'],
     zip_safe=False
